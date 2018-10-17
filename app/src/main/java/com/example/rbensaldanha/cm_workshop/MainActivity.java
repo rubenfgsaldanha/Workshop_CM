d5c2f874336a608103575b5868a06c6eef9396fd
Rúben Saldanha <rfgsaldanha@gmail.com>
2018-10-17 15:13:01 +0100

    added street view and locals

--- app/src/main/java/com/example/rbensaldanha/cm_workshop/MainActivity.java
+++ app/src/main/java/com/example/rbensaldanha/cm_workshop/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rbensaldanha.cm_workshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent startMap = new Intent(this,MapsActivity.class);
+        startActivity(startMap);
     }
 }
--- app/src/main/java/com/example/rbensaldanha/cm_workshop/MapsActivity.java
+++ app/src/main/java/com/example/rbensaldanha/cm_workshop/MapsActivity.java
@@ -3,44 +3,45 @@ package com.example.rbensaldanha.cm_workshop;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.Random;
 
-    private GoogleMap mMap;
+public class MapsActivity extends FragmentActivity implements OnStreetViewPanoramaReadyCallback {
+
+    ArrayList<LatLng> locals;
+    LatLng local;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        StreetViewPanoramaFragment streetViewPanoramaFragment = (StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id.streetViewPanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+
+        locals = new ArrayList<>();
+        //Close to Washington Monument
+        locals.add(new LatLng(38.887735, -77.032689));
+        //Close to Colosseum
+        locals.add(new LatLng(41.889129, 12.495238));
+        //Close to Taj Mahal
+        locals.add(new LatLng(27.170496,78.040306));
+        //Close to the Great Sphinx
+        locals.add(new LatLng(29.975117,31.137096));
+
+        Random rand = new Random();
+
+        int  n = rand.nextInt(locals.size());
+        local = locals.get(n);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        streetViewPanorama.setPosition(local);
     }
 }
